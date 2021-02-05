6bc9d3929cb60e1a819b2b1c68b15c0b1bf789ef
Imran Barbhuiya <74945038+imranbarbhuiya@users.noreply.github.com>
2021-02-05 17:02:10 +0530

    Added work is done style

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoItem(props) {
-  return <li> {props.item} </li>;
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prevValue) => !prevValue);
+  }
+
+  return (
+    <div onClick={handleClick}>
+      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+        {props.item}
+      </li>
+    </div>
+  );
 }
 
 export default ToDoItem;
